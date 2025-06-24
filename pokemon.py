5a263613ad44218c07b98984ff34c4e8acb066d5
Marlon <marlon.spotkaeff@gmail.com>
2025-06-24 15:26:12 +0800

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -49,10 +49,6 @@ pokeman = {
     'attack' : 70,
     'type' : 'fire',
   }
-
-
-
-
   'bulbman' = {
     'name' : 'bulbman',
     'health' : 60,
@@ -107,10 +103,6 @@ pokeman = {
     'attack' : 25,
     'type' : 'grass',
   }
-
-
-
-
   'squirtman' = {
     'name' : 'squirtman',
     'health' : 60,
@@ -153,10 +145,6 @@ pokeman = {
     'attack' : 70,
     'type' : 'water',
   }
-
-
-
-
   'pikachu' = {
     'name' : 'pikachu',
     'health' : 70,
@@ -175,11 +163,6 @@ pokeman = {
     'attack' : 70,
     'type' : 'electric',
   }
-
-
-
-
-
   'pidgman' = {
     'name' : 'pidgman',
     'health' : 50,
@@ -202,144 +185,3 @@ pokeman = {
 
 }
 
-grass_pokeman = {
-  'bulbman' = {
-    'name' : 'bulbman',
-    'health' : 60,
-    'attack' : 10,
-    'type' : 'grass',
-  }
-  'aphrosaur' = {
-    'name' : 'aphrosaur',
-    'health' : 80,
-    'attack' : 25,
-    'type' : 'grass',
-  }
-  'poisonivysaur' = {
-    'name' : 'poisonivysaur',
-    'health' : 100,
-    'attack' : 40,
-    'type' : 'grass',
-  }
-  'beelsprout' = {
-    'name' : 'beelsprout',
-    'health' : 50,
-    'attack' : 10,
-    'type' : 'grass',
-  }
-  'weepingbeel' = {
-    'name' : 'weepingbeel',
-    'health' : 70,
-    'attack' : 20,
-    'type' : 'grass',
-  }
-  'victorybeel' = {
-    'name' : 'victorybeel',
-    'health' : 120,
-    'attack' : 50,
-    'type' : 'grass',
-  }
-  'splatterpie' = {
-    'name' : 'splatterpie',
-    'health' : 40,
-    'attack' : 10,
-    'type' : 'grass',
-  }
-  'splatterpod' = {
-    'name' : 'splatterpod',
-    'health' : 90,
-    'attack' : 5,
-    'type' : 'grass',
-  }
-  'splatterfree' = {
-    'name' : 'splatterfree',
-    'health' : 80,
-    'attack' : 20,
-    'type' : 'grass',
-  }
-
-}
-
-fire_pokeman = {
-  'charman' = {
-    'name' : 'charman',
-    'health' : 60,
-    'attack' : 10,
-    'type' : 'fire',
-  }
-  'charmeleon' = {
-    'name' : 'charmelon',
-    'health' : 80,
-    'attack' : 25,
-    'type' : 'fire',
-  }
-  'charliz' = {
-    'name' : 'charliz',
-    'health' : 100,
-    'attack' : 40,
-    'type' : 'fire',
-  }
-  'minitails' = {
-    'name' : 'minitails',
-    'health' : 40,
-    'attack' : 10,
-    'type' : 'fire',
-  }
-  'tentales' = {
-    'name' = 'tentales',
-    'health' : 40,
-    'attack' : 10,
-    'type' : 'fire',
-  }
-  'moltres' = {
-    'name' : 'moltres',
-    'health' : 150,
-    'attack' : 70,
-    'type' : 'fire',
-  }
-}
-
-water_pokeman = {
-  'squirtman' = {
-    'name' : 'squirtman',
-    'health' : 60,
-    'attack' : 10,
-    'type' : 'water',
-  }
-  'warturtle' = {
-    'name' : 'warturtle',
-    'health' : 80,
-    'attack' : 25,
-    'type' : 'water',
-  }
-  'splashsaur' = {
-    'name' : 'splashsaur',
-    'health' : 100,
-    'attack' : 40,
-    'type' : 'water',
-  }
-  'giridos' = {
-    'name' : 'giridos',
-    'health' : 120,
-    'attack' : 50,
-    'type' : 'water',
-  }
-  'staryee' = {
-    'name' : 'staryee',
-    'health' : 70,
-    'attack' : 20,
-    'type' : 'water',
-  }
-  'starmee' = {
-    'name' : 'starmee',
-    'health' : 80,
-    'attack' : 30,
-    'type' : 'water',
-  }
-  'articunee' = {
-    'name' : 'articunee',
-    'health' : 150,
-    'attack' : 70,
-    'type' : 'water',
-  }
-}
