0fa0d77f23887525e0741ac3136fb1471eb74b43
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:48:41 +0800

    Update rooms.py

--- rooms.py
+++ rooms.py
@@ -1,6 +1,55 @@
 def start():
-  input('''You wake up and are forced to fight in a dungeon-style gym, against a PE teacher who smells like coffee and cigarettes.
-  What pokeman do you pick to fight his f{enemy_type} list. HINT: (fire > grass > water > fire)
+  print('''You wake up and are forced to fight in a dungeon-style gym, against a PE teacher who smells like coffee and cigarettes.
+  What pokeman do you pick to fight his f{enemy_type} list. HINT: (fire > grass > water > fire) (electric > water)
+
+  You can pick 1 Tier 3, 2 Tier 2, and 3 Tier 1 Pokeman. The tiers are displayed next to their names. 
   Fire:
-  1. Charmander
+  Charman - 1
+  Charmelon  - 1
+  Charliz - 2
+  Minitails - 1
+  Tentales - 2
+  Moltrez - 3
+
+  Grass:
+  Bulbman - 1
+  Poisonivysaur - 1
+  Aphrosaur - 2
+  Beelsprout - 1
+  Weepinbeel - 2
+  Victorybeel - 2
+  Splatterpie - 1
+  Splatterpod - 1
+  Splatterfree - 2
+
+  Water:
+  Squirtman - 1
+  Warturtle - 1
+  Splashsaur - 2
+  Giridos - 3
+  Staryee - 1
+  Starmee - 2
+  Articunee - 3
+
+  Electric:
+  Pikachu - 1
+  Raichu - 2
+  Zapdos - 3
+
+  Normal:
+  Pidgeman - 1
+  Pideetoo - 2
+  Pidgeoot - 2
+  
   ''')
+t1 = 3
+t2 = 2
+t3 = 1
+if p_p1 = input('What is your first Pokeman?').lower()
+p_p2 = input('What is your second Pokeman?').lower()
+p_p3 = input('What is your third Pokeman?').lower()
+p_p4 = input('What is your fourth Pokeman?').lower()
+p_p5 = input('What is your fifth Pokeman?').lower()
+p_p6 = input('What is your sixth Pokeman?').lower()
+
+
