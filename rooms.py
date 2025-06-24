cdef40fa2ebd23ab01f9b277e76f0974acd3053c
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:29:19 +0800

    Create rooms.py

---
+++ rooms.py
@@ -0,0 +1,6 @@
+def start():
+  input('''You wake up and are forced to fight in a dungeon-style gym, against a PE teacher who smells like coffee and cigarettes.
+  What pokeman do you pick to fight his f{enemy_type} list. HINT: (fire > grass > water > fire)
+  Fire:
+  1. Charmander
+  ''')
