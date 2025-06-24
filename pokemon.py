aedd825b66b171bdcb506107e50b104aeee8b014
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:33:39 +0800

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -15,168 +15,196 @@ def create_pokemon(hash):
 pokeman = {
   'charman' = {
     'name' : 'charman',
+    'tier' : 1,
     'health' : 60,
     'attack' : 10,
     'type' : 'fire'
   },
   'charmelon' = {
     'name' : 'charmelon',
+    'tier' : 1,
     'health' : 80,
     'attack' : 25,
     'type' : 'fire'
   }
   'charliz' = {
     'name' : 'charliz'
+    'tier' : 2,
     'health' : 100
     'attack' : 40
     'type' : 'fire'
   }
   'minitails' = {
     'name' : 'minitails',
+    'tier' : 2,
     'health' : 40,
     'attack' : 10,
     'type' : 'fire',
   }
   'tentales' = {
     'name' = 'tentales',
-    'health' : 40,
-    'attack' : 10,
+    'tier' : 2,
+    'health' : 70,
+    'attack' : 25,
     'type' : 'fire',
   }
   'moltres' = {
     'name' : 'moltres',
+    'tier' : 3,
     'health' : 150,
     'attack' : 70,
     'type' : 'fire',
   }
   'bulbman' = {
     'name' : 'bulbman',
+    'tier' : 1,
     'health' : 60,
     'attack' : 10,
     'type' : 'grass',
   }
   'aphrosaur' = {
     'name' : 'aphrosaur',
+    'tier' : 1,
     'health' : 80,
     'attack' : 25,
     'type' : 'grass',
   }
   'poisonivysaur' = {
     'name' : 'poisonivysaur',
+    'tier' : 2,
     'health' : 100,
     'attack' : 40,
     'type' : 'grass',
   }
   'beelsprout' = {
     'name' : 'beelsprout',
+    'tier' : 1,
     'health' : 50,
     'attack' : 10,
     'type' : 'grass',
   }
   'weepingbeel' = {
     'name' : 'weepingbeel',
+    'tier' : 2,
     'health' : 70,
     'attack' : 20,
     'type' : 'grass',
   }
   'victorybeel' = {
     'name' : 'victorybeel',
+    'tier' : 2,
     'health' : 120,
     'attack' : 50,
     'type' : 'grass',
   }
   'splatterpie' = {
     'name' : 'splatterpie',
+    'tier' : 1,
     'health' : 40,
     'attack' : 10,
     'type' : 'grass',
   }
   'splatterpod' = {
     'name' : 'splatterpod',
+    'tier' : 1,
     'health' : 90,
     'attack' : 5,
     'type' : 'grass',
   }
   'splatterfree' = {
     'name' : 'splatterfree',
+    'tier' : 2,
     'health' : 90,
     'attack' : 25,
     'type' : 'grass',
   }
   'squirtman' = {
     'name' : 'squirtman',
+    'tier' : 1,
     'health' : 60,
     'attack' : 10,
     'type' : 'water',
   }
   'warturtle' = {
     'name' : 'warturtle',
+    'tier' : 1,
     'health' : 80,
     'attack' : 25,
     'type' : 'water',
   }
   'splashsaur' = {
     'name' : 'splashsaur',
+    'tier' : 2,
     'health' : 100,
     'attack' : 40,
     'type' : 'water',
   }
   'giridos' = {
     'name' : 'giridos',
+    'tier' : 3,
     'health' : 120,
     'attack' : 50,
     'type' : 'water',
   }
   'staryee' = {
     'name' : 'staryee',
+    'tier' : 1,
     'health' : 70,
     'attack' : 20,
     'type' : 'water',
   }
   'starmee' = {
     'name' : 'starmee',
+    'tier' : 2,
     'health' : 80,
     'attack' : 30,
     'type' : 'water',
   }
   'articunee' = {
     'name' : 'articunee',
+    'tier' : 3,
     'health' : 150,
     'attack' : 70,
     'type' : 'water',
   }
   'pikachu' = {
     'name' : 'pikachu',
+    'tier' : 1,
     'health' : 70,
     'attack' : 20,
     'type' : 'electric',
   }
   'raichu' = {
     'name' : 'raichu',
+    'tier' : 2,
     'health' : 90,
     'attack' : 30,
     'type' : 'electric',
   }
   'zapdos' = {
     'name' : 'zapdos',
+    'tier' : 3,
     'health' : 150,
     'attack' : 70,
     'type' : 'electric',
   }
   'pidgman' = {
     'name' : 'pidgman',
+    'tier' : 1,
     'health' : 50,
     'attack' : 20,
     'type' : 'normal',
   }
   'pidgeetoo' = {
     'name' : 'pidgetoo',
+    'tier' : 2,
     'health' : 70,
     'attack' : 30,
     'type' : 'normal',
   }
   'pidgeoot' = {
     'name' : 'pidgeoot',
+    'tier' : 2,
     'health' : 100,
     'attack' : 40,
     'type' : 'normal',
