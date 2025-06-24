b08fd64f8f84761e2663dbd8248878768224140f
toby-syntax <toby.demorton@student.education.wa.edu.au>
2025-06-24 15:43:45 +0800

    Create ai_action.py

---
+++ ai_action.py
@@ -0,0 +1,19 @@
+def ai_action(inventory : list, active_pokemon : object):
+  temp_pokemon = ''
+  max_health = 0
+  win = False
+  attack = 0
+  type = ''
+  if active_pokemon.health < active_pokemon.max_health * 0.1:
+      for pokemon in inventory:
+        if pokemon.health >= max_health:
+          max_health = pokemon.health
+          temp_pokemon = pokemon
+      active_pokemon = temp_pokemon
+      if active_pokemon.health == 0: 
+        win = True
+  else:
+    attack = active_pokemon.damage
+    type = active_pokemon.type
+
+  return attack, type, active_pokemon, win
