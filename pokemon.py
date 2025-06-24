63137f92131e3e906fb69062f2034cba14827b68
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:06:43 +0800

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,345 @@
+class pokeman:
+  def __init__(self, name, health, attack, type):
+    self.name = name
+    self.max_health = health
+    self.health = health
+    self.attack = attack
+    self.type = type
+
+
+def create_pokemon(hash):
+  pokeman()
+
+
+
+pokeman = {
+  'charman' = {
+    'name' : 'charman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'fire'
+  },
+  'charmelon' = {
+    'name' : 'charmelon',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'fire'
+  }
+  'charliz' = {
+    'name' : 'charliz'
+    'health' : 100
+    'attack' : 40
+    'type' : 'fire'
+  }
+  'minitails' = {
+    'name' : 'minitails',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'fire',
+  }
+  'tentales' = {
+    'name' = 'tentales',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'fire',
+  }
+  'moltres' = {
+    'name' : 'moltres',
+    'health' : 150,
+    'attack' : 70,
+    'type' : 'fire',
+  }
+
+
+
+
+  'bulbman' = {
+    'name' : 'bulbman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'aphrosaur' = {
+    'name' : 'aphrosaur',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'grass',
+  }
+  'poisonivysaur' = {
+    'name' : 'poisonivysaur',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'grass',
+  }
+  'beelsprout' = {
+    'name' : 'beelsprout',
+    'health' : 50,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'weepingbeel' = {
+    'name' : 'weepingbeel',
+    'health' : 70,
+    'attack' : 20,
+    'type' : 'grass',
+  }
+  'victorybeel' = {
+    'name' : 'victorybeel',
+    'health' : 120,
+    'attack' : 50,
+    'type' : 'grass',
+  }
+  'splatterpie' = {
+    'name' : 'splatterpie',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'splatterpod' = {
+    'name' : 'splatterpod',
+    'health' : 90,
+    'attack' : 5,
+    'type' : 'grass',
+  }
+  'splatterfree' = {
+    'name' : 'splatterfree',
+    'health' : 90,
+    'attack' : 25,
+    'type' : 'grass',
+  }
+
+
+
+
+  'squirtman' = {
+    'name' : 'squirtman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'water',
+  }
+  'warturtle' = {
+    'name' : 'warturtle',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'water',
+  }
+  'splashsaur' = {
+    'name' : 'splashsaur',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'water',
+  }
+  'giridos' = {
+    'name' : 'giridos',
+    'health' : 120,
+    'attack' : 50,
+    'type' : 'water',
+  }
+  'staryee' = {
+    'name' : 'staryee',
+    'health' : 70,
+    'attack' : 20,
+    'type' : 'water',
+  }
+  'starmee' = {
+    'name' : 'starmee',
+    'health' : 80,
+    'attack' : 30,
+    'type' : 'water',
+  }
+  'articunee' = {
+    'name' : 'articunee',
+    'health' : 150,
+    'attack' : 70,
+    'type' : 'water',
+  }
+
+
+
+
+  'pikachu' = {
+    'name' : 'pikachu',
+    'health' : 70,
+    'attack' : 20,
+    'type' : 'electric',
+  }
+  'raichu' = {
+    'name' : 'raichu',
+    'health' : 90,
+    'attack' : 30,
+    'type' : 'electric',
+  }
+  'zapdos' = {
+    'name' : 'zapdos',
+    'health' : 150,
+    'attack' : 70,
+    'type' : 'electric',
+  }
+
+
+
+
+
+  'pidgman' = {
+    'name' : 'pidgman',
+    'health' : 50,
+    'attack' : 20,
+    'type' : 'normal',
+  }
+  'pidgeetoo' = {
+    'name' : 'pidgetoo',
+    'health' : 70,
+    'attack' : 30,
+    'type' : 'normal',
+  }
+  'pidgeoot' = {
+    'name' : 'pidgeoot',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'normal',
+  }
+
+
+}
+
+grass_pokeman = {
+  'bulbman' = {
+    'name' : 'bulbman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'aphrosaur' = {
+    'name' : 'aphrosaur',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'grass',
+  }
+  'poisonivysaur' = {
+    'name' : 'poisonivysaur',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'grass',
+  }
+  'beelsprout' = {
+    'name' : 'beelsprout',
+    'health' : 50,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'weepingbeel' = {
+    'name' : 'weepingbeel',
+    'health' : 70,
+    'attack' : 20,
+    'type' : 'grass',
+  }
+  'victorybeel' = {
+    'name' : 'victorybeel',
+    'health' : 120,
+    'attack' : 50,
+    'type' : 'grass',
+  }
+  'splatterpie' = {
+    'name' : 'splatterpie',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'grass',
+  }
+  'splatterpod' = {
+    'name' : 'splatterpod',
+    'health' : 90,
+    'attack' : 5,
+    'type' : 'grass',
+  }
+  'splatterfree' = {
+    'name' : 'splatterfree',
+    'health' : 80,
+    'attack' : 20,
+    'type' : 'grass',
+  }
+
+}
+
+fire_pokeman = {
+  'charman' = {
+    'name' : 'charman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'fire',
+  }
+  'charmeleon' = {
+    'name' : 'charmelon',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'fire',
+  }
+  'charliz' = {
+    'name' : 'charliz',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'fire',
+  }
+  'minitails' = {
+    'name' : 'minitails',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'fire',
+  }
+  'tentales' = {
+    'name' = 'tentales',
+    'health' : 40,
+    'attack' : 10,
+    'type' : 'fire',
+  }
+  'moltres' = {
+    'name' : 'moltres',
+    'health' : 150,
+    'attack' : 70,
+    'type' : 'fire',
+  }
+}
+
+water_pokeman = {
+  'squirtman' = {
+    'name' : 'squirtman',
+    'health' : 60,
+    'attack' : 10,
+    'type' : 'water',
+  }
+  'warturtle' = {
+    'name' : 'warturtle',
+    'health' : 80,
+    'attack' : 25,
+    'type' : 'water',
+  }
+  'splashsaur' = {
+    'name' : 'splashsaur',
+    'health' : 100,
+    'attack' : 40,
+    'type' : 'water',
+  }
+  'giridos' = {
+    'name' : 'giridos',
+    'health' : 120,
+    'attack' : 50,
+    'type' : 'water',
+  }
+  'staryee' = {
+    'name' : 'staryee',
+    'health' : 70,
+    'attack' : 20,
+    'type' : 'water',
+  }
+  'starmee' = {
+    'name' : 'starmee',
+    'health' : 80,
+    'attack' : 30,
+    'type' : 'water',
+  }
+  'articunee' = {
+    'name' : 'articunee',
+    'health' : 150,
+    'attack' : 70,
+    'type' : 'water',
+  }
+}
