bdf47be5b1c7eebc053c77f42d4b92af03759361
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:08:05 +0800

    Update combat.py

--- combat.py
+++ combat.py
@@ -18,3 +18,24 @@ def combat(playerType):
     elif playerType == "Electric":
         playerWeakness = ["Electric", "Grass"]
         playerStrength = ["Water"]
+
+
+def playerDamageCalc(enemyType, enemyHealth, playerAttack):
+    damage = playerAttack
+    if enemyType in playerStrength:
+        damage *= 2
+    elif enemyType in playerWeakness:
+        damage /= 2
+    enemyHealth -= damage
+    print(f"[Player name] deals {damage} damage to [Enemy name].")
+    return enemyHealth
+
+def enemyDamageCalc(enemyType, playerHealth, enemyAttack):
+    damage = enemyAttack
+    if enemyType in playerStrength:
+        damage /= 2
+    elif enemyType in playerWeakness:
+        damage *= 2
+    playerHealth -= damage
+    print(f"[Enemy name] deals {damage} damage to [Player name].")
+    return playerHealth
