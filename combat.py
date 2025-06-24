1d6a6355bfba375d4fbdaefc85db1f61cacbcc29
Rachogonnawin <rachit.gujarati@student.education.wa.edu.au>
2025-06-24 15:07:46 +0800

    Update combat.py

--- combat.py
+++ combat.py
@@ -1,1 +1,20 @@
+def combat(playerType):
+    if playerType == "Fire":
+        playerWeakness = ["Water", "Rock", "Fire"]
+        playerStrength = ["Grass"]
 
+    elif playerType == "Water":
+        playerWeakness = ["Water", "Grass"]
+        playerStrength = ["Fire", "Rock"]
+
+    elif playerType == "Grass":
+        playerWeakness = ["Fire", "Grass"]
+        playerStrength = ["Water", "Rock"]
+
+    elif playerType == "Rock":
+        playerWeakness = []
+        playerStrength = ["Fire"]
+    
+    elif playerType == "Electric":
+        playerWeakness = ["Electric", "Grass"]
+        playerStrength = ["Water"]
